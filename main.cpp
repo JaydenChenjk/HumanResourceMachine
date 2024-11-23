13fa6f9278748812d0a0219cf4f9e6cbe6c3b145
JaydenChenjk <cjk24@mails.tsinghua.edu.cn>
2024-11-23 18:03:44 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,21 +9,32 @@ vector<int> input;
 vector<int> target;
 int current_block;
 vector<int> output;
-vector<string> availables;
 vector<int> spaces;
 int instruction_count;
 int index = 0;
 
-void load_availables_from_keyboard() {
+struct INSTRUCTIONS {
+    string name;
+    int number;
+};
+vector<INSTRUCTIONS> instructions(instruction_count);
+
+void load_instructions_from_keyboard() {
     try {
-        cout << "Enter the number of availables: ";
+        cout << "Enter the number of instructions: ";
         cin >> instruction_count;
         cin.ignore();
-        cout << "Enter your availables one by one (e.g., inbox, outbox):" << endl;
+        cout << "Enter your instructions one by one (e.g., inbox, outbox):" << endl;
         for (int i = 0; i < instruction_count; i++) {
             string instruction;
             getline(cin, instruction);
-            availables.push_back(instruction);
+            for(int j=0;j<instruction.length();j++) {
+                if(instruction[j]==' ') {
+                    instructions[i].number=instruction[j+1];
+                    break;
+                }
+                instructions[i].name+=instruction[j];
+            }
         }
     }
     catch (...) {
@@ -31,7 +42,7 @@ void load_availables_from_keyboard() {
     }
 }
 
-void load_availables_from_file(const string& file_path) {
+void load_instructions_from_file(const string& file_path) {
     try {
         ifstream file(file_path);
         if (!file) {
@@ -40,9 +51,17 @@ void load_availables_from_file(const string& file_path) {
         }
         file >> instruction_count;
         file.ignore();
-        availables.resize(instruction_count);
+        instructions.resize(instruction_count);
         for (int i = 0; i < instruction_count; i++) {
-            getline(file, availables[i]);
+            string instruction;
+            getline(cin, instruction);
+            for(int j=0;j<instruction.length();j++) {
+                if(instruction[j]==' ') {
+                    instructions[i].number=instruction[j+1];
+                    break;
+                }
+                instructions[i].name+=instruction[j];
+            }
         }
     }
     catch (...) {
@@ -51,13 +70,13 @@ void load_availables_from_file(const string& file_path) {
 }
 
 void inbox(int i) {
-        if (index < input.size()) {
-            current_block = input[index];
-            index++;
-        }
-        else
-            cout << "Error on instruction " << (i + 1) << endl;
+    if (index < input.size()) {
+        current_block = input[index];
+        index++;
     }
+    else
+        cout << "Error on instruction " << (i + 1) << endl;
+}
 
 void outbox(int i) {
     if (current_block != -1) {
@@ -71,12 +90,15 @@ void outbox(int i) {
 
 
 void run1() {
-    for (int i = 0; i < availables.size(); i++) {
-        cout << (i + 1) << " " << availables[i] << endl;
+    for (int i = 0; i < instructions.size(); i++) {
+        cout << (i + 1) << " " << instructions[i].name;
+        if(instructions[i].number!=-1)
+            cout << " " << instructions[i].number;
+        cout << endl;
 
-        if (availables[i] == "inbox")
+        if (instructions[i].name == "inbox")
             inbox(i);
-        else if (availables[i] == "outbox")
+        else if (instructions[i].name == "outbox")
             outbox(i);
         else
             cout << "Error on instruction " << (i + 1) << endl;
@@ -89,24 +111,46 @@ void run1() {
 }
 
 void run2() {
+    for (int i = 0; i < instructions.size(); i++) {
+        cout << (i + 1) << " " << instructions[i].name;
+        if(instructions[i].number!=-1)
+            cout << " " << instructions[i].number;
+        cout << endl;
 
+        if (instructions[i].name == "inbox")
+            inbox(i);
+        else if (instructions[i].name == "outbox")
+            outbox(i);
+        else if (instructions[i].name == "add")
+            add(i);
+        else
+            cout << "Error on instruction " << (i + 1) << endl;
+    }
+
+    if (output == target)
+        cout << "Success" << endl;
+    else
+        cout << "Fail" << endl;
 }
 
 int main() {
     vector<int> input = {1, 2};
     vector<int> target = {1, 2};
+    for(int i=0;i<instruction_count;i++) {
+        instructions[i].name=-1;
+    }
 
     string mode;
     cout << "Choose input mode (keyboard/file): ";
     cin >> mode;
     cin.ignore();
     if (mode == "keyboard")
-        load_availables_from_keyboard();
+        load_instructions_from_keyboard();
     else if (mode == "file") {
         string file_path;
         cout << "Enter the file path: ";
         cin >> file_path;
-        load_availables_from_file(file_path);
+        load_instructions_from_file(file_path);
     }
     else {
         cout << "Invalid mode." << endl;
