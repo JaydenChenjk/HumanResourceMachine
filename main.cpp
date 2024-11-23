171e921b7b04516df225fcc8068cd0964be263f4
JaydenChenjk <cjk24@mails.tsinghua.edu.cn>
2024-11-23 17:18:41 +0800

    1(version 2)

--- main.cpp
+++ main.cpp
@@ -5,83 +5,92 @@
 
 using namespace std;
 
-    vector<int> input;
-    vector<int> target;
-    int current_block;
-    vector<int> output;
-    vector<string> availables;
-    int instruction_count;
+vector<int> input;
+vector<int> target;
+int current_block;
+vector<int> output;
+vector<string> availables;
+vector<int> spaces;
+int instruction_count;
+int index = 0;
 
-
-    void load_availables_from_keyboard() {
-        try {
-            cout << "Enter the number of availables: ";
-            cin >> instruction_count;
-            cin.ignore();
-            cout << "Enter your availables one by one (e.g., inbox, outbox):" << endl;
-            for (int i = 0; i < instruction_count; i++) {
-                string instruction;
-                getline(cin, instruction);
-                availables.push_back(instruction);
-            }
-        }
-        catch (...) {
-            cout << "Invalid input." << endl;
+void load_availables_from_keyboard() {
+    try {
+        cout << "Enter the number of availables: ";
+        cin >> instruction_count;
+        cin.ignore();
+        cout << "Enter your availables one by one (e.g., inbox, outbox):" << endl;
+        for (int i = 0; i < instruction_count; i++) {
+            string instruction;
+            getline(cin, instruction);
+            availables.push_back(instruction);
         }
     }
+    catch (...) {
+        cout << "Invalid input." << endl;
+    }
+}
 
-    void load_availables_from_file(const string& file_path) {
-        try {
-            ifstream file(file_path);
-            if (!file) {
-                cout << "File not found: " << file_path << endl;
-                return;
-            }
-            file >> instruction_count;
-            file.ignore();
-            availables.resize(instruction_count);
-            for (int i = 0; i < instruction_count; i++) {
-                getline(file, availables[i]);
-            }
+void load_availables_from_file(const string& file_path) {
+    try {
+        ifstream file(file_path);
+        if (!file) {
+            cout << "File not found: " << file_path << endl;
+            return;
         }
-        catch (...) {
-            cout << "Invalid file format." << endl;
+        file >> instruction_count;
+        file.ignore();
+        availables.resize(instruction_count);
+        for (int i = 0; i < instruction_count; i++) {
+            getline(file, availables[i]);
         }
     }
+    catch (...) {
+        cout << "Invalid file format." << endl;
+    }
+}
 
-    void run1() {
-        int input_index = 0;
-        for (int i = 0; i < availables.size(); i++) {
-            cout << (i + 1) << " " << availables[i] << endl;
-
-            if (availables[i] == "inbox") {
-                if (input_index < input.size()) {
-                    current_block = input[input_index];
-                    input_index++;
-                    cout << "inbox" << endl;
-                }
-                else
-                    cout << "Error on instruction " << (i + 1) << endl;
-            }
-            else if (availables[i] == "outbox") {
-                if (current_block != -1) {
-                    output.push_back(current_block);
-                    cout << "output" << endl;
-                    current_block = -1;
-                }
-                else
-                    cout << "Error on instruction " << (i + 1) << endl;
-            }
-            else
-                cout << "Error on instruction " << (i + 1) << endl;
+void inbox(int i) {
+        if (index < input.size()) {
+            current_block = input[index];
+            index++;
         }
-
-        if (output == target)
-            cout << "Success" << endl;
         else
-            cout << "Fail" << endl;
+            cout << "Error on instruction " << (i + 1) << endl;
     }
 
+void outbox(int i) {
+    if (current_block != -1) {
+        output.push_back(current_block);
+        current_block = -1;
+    }
+    else
+        cout << "Error on instruction " << (i + 1) << endl;
+}
+
+
+
+void run1() {
+    for (int i = 0; i < availables.size(); i++) {
+        cout << (i + 1) << " " << availables[i] << endl;
+
+        if (availables[i] == "inbox")
+            inbox(i);
+        else if (availables[i] == "outbox")
+            outbox(i);
+        else
+            cout << "Error on instruction " << (i + 1) << endl;
+    }
+
+    if (output == target)
+        cout << "Success" << endl;
+    else
+        cout << "Fail" << endl;
+}
+
+void run2() {
+
+}
 
 int main() {
     vector<int> input = {1, 2};
