80c0e2a7f05765c23b04f5f380458914fd6cde48
cherriberry <107687472+cherriberry@users.noreply.github.com>
2024-12-10 22:33:16 +0800

    Add files via upload

---
+++ game1.2.cpp
@@ -0,0 +1,477 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+struct INSTRUCTIONS {
+    string name;//否则后面初始化不好初始化
+    int number;
+};
+//全局变量设定：
+bool user_levels[5]={1};//存储用户关卡通关信息，关卡号就是下标！
+bool user_levels_lock[5] = {1};//为了方便，从1开始记录！
+ifstream fusers_in,flevel_in;//读取用户数据文件、关卡信息
+ofstream fusers_out,fusers_lstins;//更新电脑中的存档信息
+bool faults = 0;//程序是否出错（也许可以记录一下是什么错？）
+vector<INSTRUCTIONS> last_instructions;
+
+vector<int> input;//输入
+vector<int> target;//目标输出
+vector<int> output;
+vector<string> availables;//可用命令
+vector<INSTRUCTIONS> instructions;
+vector<int> spaces;//空间数
+string fusers;//用户数据文件
+string levels;//关卡名
+int instruction_count = 0;
+int current_block = NULL;
+int indexx = 0;
+int level;
+int spnum = 3;//空间数
+bool succeed = 0 ,quit = 0;
+
+
+int Load_menu()//载入目录数据
+{
+    cout<<"请输入文件路径"<<endl;
+    getline(cin, fusers);
+    fusers_in.open(fusers);//打开文件
+    try 
+    {
+       if(!fusers_in)
+        {
+            cout<<"无法打开文件！请检查文件是否存在以及路径是否正确。"<<endl;
+            return -1;
+        }
+        else
+        {
+            int lvname;//关卡号
+            bool lv_is_pass;//此关卡之前是否通关
+            while(fusers_in>>lvname>>lv_is_pass)
+                user_levels[lvname]=lv_is_pass;//存储用户通关数据 用户数据格式：关卡号+是否通关
+            
+            for(int i = 1;i<4;i++)//用户可选关数据，关卡号就是下标
+            {
+                if(user_levels[i-1])
+                    user_levels_lock[i] = 1;
+                else
+                    user_levels_lock[i] = 0;
+            }
+            fusers_in.close();
+            return 0;
+        }
+    }
+    catch (...)//处理任何异常
+    {
+        cout << "Invalid file format." << endl;
+        fusers_in.close();
+        return -1;
+    }
+
+}
+
+void Menu()//展示目录
+{
+    cout<<endl<<"Menu"<<endl;
+    for(int i = 1;i<4;i++)
+    {
+        cout<<"level "<<i;
+        if(!user_levels_lock[i])
+            cout<<"------locked";
+        else
+        {
+            if(user_levels[i])
+                cout<<"------ ●";
+            else
+                cout<<"------ ○ ";
+        }
+        cout<<endl;
+    }
+}
+
+int Load_init_data(vector<string> &inputs)//以string形式存储所有数据
+{
+    string counts;//总共数据数（行数！所以input和output需要多行存储）
+    int count;//转化为int
+    getline(flevel_in, counts);//可能出错！用全局变量接着读可能会出错，后续排查！//需要先输入有几个值！
+    count = stoi(counts);
+    string in;
+    for (int i = 0; i < count; i++) 
+    {
+        getline(flevel_in, in);//存储的时候一行存多个数值！
+        inputs.push_back(in);
+    }
+    return count;
+}
+
+void Load_init_int(vector<int> &inputn)
+{
+    vector<string> inputs;
+    int count;
+    count = Load_init_data(inputs);
+    inputn.resize(count);
+    for(int i = 0;i<count;i++)
+        inputn[i]=stoi(inputs[i]);
+}
+
+void load_instructions_from_keyboard() {
+    try {
+        cout << "Enter the number of instructions: ";
+        cin >> instruction_count;
+        cin.ignore();
+        instructions.resize(instruction_count);
+
+        cout << "Enter your instructions one by one (e.g., inbox, outbox):" << endl;
+        for (int i = 0; i < instruction_count; i++) {
+            string instruction;
+            string tmps;//临时存放所得指令的字符串，省去思考怎么初始化instructions[i].name的麻烦，但如果有时间可以尝试省去这个变量
+            getline(cin, instruction);
+            for(int j=0;j<instruction.length();j++) {
+                if(instruction[j]==' ') {
+                    instructions[i].number=instruction[j+1];
+                    break;
+                }
+                tmps+=instruction[j];
+            }
+            instructions[i].name = tmps;
+        }
+    }
+    catch (...) {
+        cout << "Invalid input." << endl;
+    }
+}
+
+
+void load_instructions_from_file(const string& file_path, vector<INSTRUCTIONS>instr) {
+    int tmp;
+    try {
+        ifstream file(file_path);
+        if (!file) {
+            cout << "File not found: " << file_path << endl;
+            return;
+        }
+        file>>tmp;
+        if(file.eof())//可能用户第一次玩，没有数据
+            return;
+        
+        instruction_count = 0;//加一个归零防止崩溃
+        file >> instruction_count;
+        file.ignore();
+        instr.resize(instruction_count);
+        for (int i = 0; i < instruction_count; i++) {
+            string instruction;
+            getline(cin, instruction);
+            for(int j=0;j<instruction.length();j++) {
+                if(instruction[j]==' ') {
+                   instr[i].number=instruction[j+1];
+                    break;
+                }
+               instr[i].name+=instruction[j];
+            }
+        }
+        file.close();
+    }
+    catch (...) {
+        cout << "Invalid file format." << endl;
+    }
+}
+
+int initial(/*vector<int> &input,vector<int> &target,vector<string> &availables*/)//初始化函数，关卡数据存储格式：输入函数、目标、可用指令；
+{
+    string instr;//存储指令
+    
+    cout<<"Please enter the number of the level you want to play.\nEnter Quit to quit the game"<<endl;
+    cin>>levels;
+
+    if(levels == "Quit")//退出游戏
+    {
+        quit = 1;
+        return -1;
+    }
+
+    try {
+        string path = "C:\\Users\\fyh_1\\Desktop\\programme\\cxsjjc\\Ming's robot\\level"+levels+".txt";
+        flevel_in.open("C:\\Users\\fyh_1\\Desktop\\programme\\cxsjjc\\Ming's robot\\level0.txt");
+        flevel_in.close();
+        flevel_in.open(path);//打开关卡存储文件
+        if(!flevel_in)//判断能否打开;但如果打开路径和原本的路径一样，没有“打开”操作，所以会认为“打开失败”
+        {
+            cout<<"Unable to find this level! Please check if there's any spelling mistake."<<endl;
+            return -1;
+        }
+
+        else
+        {
+            level = stoi(levels);//转化关卡号为数字 
+            if(levels == "1")
+                level = 1;
+            
+            if(level<1||level>5)//输入关卡号错误情况
+            {
+                cout<<"关卡号输入错误！请重新输入。";
+                return -1;//如果为-1则一直在循环里！（do_while)  
+            }
+            else if(!user_levels_lock[level])
+            {
+                cout<<"This level is locked! You need to pass all the previous levels to unlock."<<endl;
+                flevel_in.close();
+                return -1;
+            }
+
+            else
+            {
+                string tmp;
+                Load_init_int(input);
+                getline(flevel_in, tmp);//中间有空行，所以消除一下
+                
+                Load_init_int(target);
+                getline(flevel_in, tmp);//中间有空行，所以消除一下
+                
+                Load_init_data(availables);
+                getline(flevel_in, tmp);
+                
+                flevel_in>>spnum;//获取空格数
+                flevel_in>>tmp;
+            
+                if(!flevel_in.eof())
+                    cout<<"Please don't change this file!"<<endl;
+                flevel_in.close();
+                return spnum;
+            }
+        }
+
+    }
+    catch (...) {
+        cout << "Invalid file format." << endl;
+        flevel_in.close();
+        return -1;
+    }
+
+}
+
+void show_init()
+{
+    cout<<"\ninputs:"<<endl;
+    for(int i = 0;i<input.size();i++)
+    {
+        cout<<input[i]<<" ";
+    }
+    cout<<"\ntargets\n";
+    for(int i = 0;i<target.size();i++)
+    {
+        cout<<target[i]<<" ";
+    }
+    cout<<"\navailables:"<<endl;
+    for(int i = 0;i<availables.size();i++)
+    {
+        cout<<availables[i]<<" ";
+    }
+    cout<<"\navailable spaces:"<<spnum<<endl;
+
+    cout<<"Last instructions:"<<endl;//此时还是记录上一次退出时的ins数量！
+    if(instruction_count == 0)
+        cout<<"None"<<endl;
+    else{
+        for(int i = 0;i<instruction_count;i++)
+        {
+            cout<<last_instructions[i].name<<" "<<last_instructions[i].number<<endl;
+        }
+    }
+}
+
+void upload_data(string lvplayed)//每一次退出关卡都运行一遍！
+{
+    fusers_out.open(fusers);//这个就是新建文件了，在打开的一瞬间就会覆盖（？）
+    try 
+    {
+       if(!fusers_out)
+        {
+            cout<<"无法打开文件！请检查文件是否存在以及路径是否正确。"<<endl;
+        }
+        else
+        {
+            int lvname = 1;//关卡号
+            for(;lvname<4;lvname++)
+                fusers_out<<lvname<<" "<<user_levels[lvname]<<endl;
+                //存储用户通关数据 用户数据格式：关卡号+ +是否通关
+            
+            fusers_lstins.open("C:\\Users\\fyh_1\\Desktop\\programme\\cxsjjc\\Ming's robot\\user_level"+ lvplayed +".txt");
+            fusers_lstins<<instruction_count<<endl;
+            for (int i = 0; i < instruction_count; i++) //记录指令
+                fusers_lstins<<instructions[i].name<<" "<<instructions[i].number<<endl;
+                fusers_lstins.close();
+        }
+    }
+    catch (...)//处理任何异常
+    {
+        cout << "Invalid file format." << endl;
+    }
+    fusers_out.close();
+
+}
+
+
+void user_input_cons()
+{
+    string mode;
+    cout << "Choose input mode (keyboard/file): ";
+    cin >> mode;
+    cin.ignore();
+    if (mode == "keyboard")
+        load_instructions_from_keyboard();
+    else if (mode == "file") {
+        string file_path;
+        cout << "Enter the file path: ";
+        cin >> file_path;
+        load_instructions_from_file(file_path,instructions);
+    }
+    else {
+        cout << "Invalid mode." << endl;
+        return ;
+    }
+}
+
+void inbox(int i) {
+    if (indexx < input.size()) {
+        current_block = input[indexx];
+        indexx++;
+    }
+    else
+        cout << "Error on instruction " << (i + 1) << endl;
+}
+
+void outbox(int i) {
+    if (current_block != NULL) {
+        output.push_back(current_block);
+        current_block = NULL;
+    }
+    else
+        cout << "Error on instruction " << (i + 1) << endl;
+}
+
+
+
+bool run1() {
+    for (int i = 0; i < instructions.size(); i++) {
+        cout << (i + 1) << " " << instructions[i].name;
+        if(instructions[i].number!=-1)
+            cout << " " << instructions[i].number;
+        cout << endl;
+
+        if (instructions[i].name == "inbox")
+            inbox(i);
+        else if (instructions[i].name == "outbox")
+            outbox(i);
+        else
+            cout << "Error on instruction " << (i + 1) << endl;//是不是可以直接break？
+    }
+
+    if (output == target)
+    {
+        cout << "Success" << endl;
+        return 1;
+    }
+
+    else
+    {
+        cout << "Fail" << endl;
+        return 0;
+    }
+
+}
+
+void run2() {
+    for (int i = 0; i < instructions.size(); i++) {
+        cout << (i + 1) << " " << instructions[i].name;
+        if(instructions[i].number!=-1)
+            cout << " " << instructions[i].number;
+        cout << endl;
+
+        if (instructions[i].name == "inbox")
+            inbox(i);
+        else if (instructions[i].name == "outbox")
+            outbox(i);
+   //     else if (instructions[i].name == "add")
+ //           add(i);
+        else
+            cout << "Error on instruction " << (i + 1) << endl;
+    }
+
+    if (output == target)
+        cout << "Success" << endl;
+    else
+        cout << "Fail" << endl;
+}
+
+
+
+
+
+
+int main()
+{
+    //原本input、target、availables、spaces在这里定义
+    int a;
+    string retry;
+    //初始化
+    do
+    {
+        a =Load_menu();//加载目录
+    } while (!(a+1));
+    while(!quit)
+    {
+        upload_data(levels);
+
+        Menu();//显示目录
+
+        int spnum;//初始化;spnum:space number
+        do{
+            spnum = initial(/*input,target,availables*/);
+        } while(!(spnum+1)&& !quit);
+        
+
+        if(quit)//退出游戏
+            break;
+        
+        spaces.resize(spnum);
+
+//正式开始运行关卡
+        succeed = 0;     
+        while(!succeed)//游戏进行中
+        {
+
+            const string last_inp = "C:\\Users\\fyh_1\\Desktop\\programme\\cxsjjc\\Ming's robot\\user_level"+levels+".txt";
+            load_instructions_from_file(last_inp,last_instructions);//按行存储上一次的指令
+
+            show_init();//显示界面
+
+            user_input_cons();
+            succeed = 0;
+            succeed = run1();
+
+            if(succeed)
+            {
+                user_levels[level] = 1;
+                break;
+            }
+            
+            else//失败重新尝试
+            {
+                cout<<"Enter R to retry."<<endl;//文件输入暂时无法实现retry功能
+                cin>>retry;
+                if(retry == "R")
+                    continue;
+                else
+                    break;
+            }
+        }
+    }
+    
+
+    return 0;
+}
+//之前：读取数据；
+//输入关卡-》通过关卡号初始化待输入队列、目标输出、可用指令集-》转接进入关卡；
+//之后：用文件形式保存数据，这个还没做
